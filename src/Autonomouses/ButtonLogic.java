cefa655fc713401b5cdf7ed12fa296efa7fa746f
mckillip <bob@continuum-dynamics.com>
2018-03-23 01:25:17 -0400

    Autonomous tank drive with buttons
    Tank drive with feedback options and recorded joystick buttons for game piece manipulation.

---
+++ src/Autonomouses/ButtonLogic.java
@@ -0,0 +1,96 @@
+package Autonomouses;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * ButtonLogic
+ * This class stores the commmand and operational logic that is "connected" to a joystick button,
+ * as assigned within the OI.java class.  The idea is that a recorded button press can be processed
+ * with this class to execute (or not) the appropriate command function from a replay file.
+ * The goal is to provide button-press replay functionality from a file that has recorded
+ * actual robot motion and button pressing when executing a path and command history that is to
+ * be replayed during an autonomous period.  Goals are good.
+ * @author bob
+ *
+ */
+public class ButtonLogic {
+//	int assignedButton; // integer for desired button assignment
+	int buttonState = 0; // button state recorded at completion of this task's method
+	int opState = 0; // records operating (1) or dormant (0) state of associated command for toggles
+	Command cmd = null; // operating command for execution (or not) from recorded button state
+	int logicType; // stored operational mode for executing this command
+	public final static int WHILEHELD = 1;
+	public final static int WHENPRESSED = 2;
+	public final static int WHENRELEASED = 3;
+	public final static int TOGGLEWHENPRESSED = 4;
+
+	// Creator assigns internal variables
+	public void ButtonLogic( Command theCmd, int theLogic, int theButton ) {
+		assignedButton = theButton;
+		logicType = theLogic;
+		cmd = theCmd;
+	}
+
+	// "Workalike" methods for assigning functionality that look like JoystickButton methods
+	public void whileHeld( Command theCmd ) {
+		cmd = theCmd;
+		logicType = WHILEHELD;
+	}
+	public void whenPressed( Command theCmd ) {
+		cmd = theCmd;
+		logicType = WHENPRESSED;
+	}
+	public void whenReleased( Command theCmd ) {
+		cmd = theCmd;
+		logicType = WHENRELEASED;
+	}
+	public void toggleWhenPressed( Command theCmd ) {
+		cmd = theCmd;
+		logicType = TOGGLEWHENPRESSED;
+	}
+	
+	// Execution fires commands based on button state
+	public void applyButton( int newButtonState ) {
+		// If we didn't assign a command (the button is not used?),
+		// then just exit.
+		if ( null == cmd ) {
+			return;
+		}
+		// Processing logic uses a switch statement
+		switch (logicType) {
+		
+		case WHILEHELD:
+			if ( newButtonState > 0 ) {
+				cmd.start();
+			}
+			break;
+			
+		case WHENPRESSED:
+			if (( buttonState == 0 )&&( newButtonState == 1 )) { // rising edge
+				cmd.start();
+			}
+			break;
+			
+		case WHENRELEASED:
+			if (( buttonState == 1 )&&( newButtonState == 0 )) { // falling edge
+				cmd.start();
+			}
+			break;
+
+		case TOGGLEWHENPRESSED:
+			if (( buttonState == 0 )&&( newButtonState == 1 )) { // rising edge
+				if ( opState == 0 ) {
+					cmd.start();
+					opState = 1;
+				} else {
+					cmd.interrupt();
+					opState = 0;
+				}
+			}
+			break;
+		default:
+		}
+		buttonState = newButtonState;
+	}
+	
+}
---
+++ src/Autonomouses/DriveTankReplay.java
@@ -0,0 +1,99 @@
+package org.usfirst.frc.team293.robot.commands;
+
+import org.usfirst.frc.team293.robot.OI;
+import org.usfirst.frc.team293.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import Autonomouses.ReplayFile;
+
+/**
+ *
+ */
+public class DriveTankReplay extends Command {
+	private double left;
+	private double right;
+	private double outleft;
+	private double outright;
+	
+	public ReplayFile replayFile = new ReplayFile();
+	
+    public DriveTankReplay() {
+    	requires(Robot.driveTrain);// Use requires() here to declare subsystem dependencies
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	replayFile.init(fnum); // opens file for auto following, gets first two text lines
+    }
+    
+    /**
+     * Method to convert single slope linear input from joystick into linear input using 2 lines of different slope
+     * @param right Raw input from right joystick from -1 to 1
+     * @param left Raw input from left joystick from -1 to 1
+     * @return Array containing processed left and right inputs 
+     */
+    public double[] stickCooker(double right, double left){
+    	
+		return new double[] {outright, outleft};
+    }
+    
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+//    	right = OI.rightStick.getY();
+//    	left  = OI.leftStick.getY();
+    	left  = replayFile.chanValues[1]; // joysticks; index 0 is time hack
+    	right = replayFile.chanValues[2];
+    	double left_enc  = replayFile.chanValues[3]; 
+    	double right_enc = replayFile.chanValues[4];
+    	double yaw       = replayFile.chanValues[5];
+    	double yawRate   = replayFile.chanValues[6];
+
+    	// Define constants for "cooked" joystick drive inputs to give creep/leap response
+    	double in1 = 0.35;
+    	double out1 = 0.25;
+    	double ls = Math.signum(left); //gets signs of both inputs
+    	double rs = Math.signum(right);
+    	if ( Math.abs(right) < in1 ) {
+    		outright = right * (out1 / in1 ); // low gain range
+    	} else { 
+    		outright = rs * (out1 + ( (rs * right)- in1 )*(1-out1)/(1-in1));
+    	}
+    	if ( Math.abs(left) < in1 ) {
+    		outleft = left * (out1 / in1 ); // low gain range
+    	} else { 
+    		outleft = ls * (out1 + ( (ls * left)- in1 )*(1-out1)/(1-in1));
+    	}
+    	
+    	// Stick input recreated from file data, now add feedback!
+    	double l_enc_err = Robot.driveTrain.leftEncoder.getDistance() - left_enc;
+    	double r_enc_err = Robot.driveTrain.rightEncoder.getDistance() - right_enc;
+    	double ang_error = (Robot.driveTrain.pigeonImu.getFusedHeading() - yaw0) - yaw;
+    	double Kp = 0.0; // make nonzero for feedback!
+    	double Ka = 0.0;
+    	outleft  += (Kp * l_enc_err) + (Ka * ang_error/2.);
+    	outright += (Kp * r_end_err) - (Ka * ang_error/2.);
+    	// Baby won't you drive my car? Yes, I'm going to be a star!
+    	Robot.driveTrain.feedForwardEncoderDrive((-1*outleft), (-1*outright));
+    	
+    	// Button button who's got the button?  Time to make the other stuff move.
+    	for (int i=1; i< OI.numButtonFunctions; i++) {
+    		OI.theButtons[i].applyButton( (int) replayFile.chanValues[6+i] );
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
